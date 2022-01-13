f937fa1d04b292bdad0dc1a71884721634123735
Felipe Sere <github@felipesere.com>
2022-01-13 08:29:55 +0000

    Show the git sha and repo status in version

--- build.rs
+++ build.rs
@@ -2,16 +2,33 @@ use std::env;
 
 fn main() {
     let src = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let dirty = built::util::get_repo_description(src.as_ref())
+        .ok()
+        .flatten()
+        .map(|(_, dirty)| dirty)
+        .unwrap_or(false);
+
     let sha = built::util::get_repo_head(src.as_ref())
         .ok()
         .flatten()
-        .map(|(_, sha)| sha)
+        .map(|(_, mut sha)| {
+            sha.truncate(7);
+            sha
+        })
+        .map(|sha| {
+            if dirty {
+                format!("{}-dirty", sha)
+            } else {
+                sha
+            }
+        })
         .or_else(|| std::env::var("GITHUB_SHA").ok())
         .unwrap_or_else(|| "none".into());
 
     let pkg_version = env!("CARGO_PKG_VERSION");
     println!(
-        r#"cargo:rustc-env=FANCY_VERSION="{} ({})""#,
+        r#"cargo:rustc-env=FANCY_VERSION={} ({})"#,
         pkg_version, sha
     )
 }
